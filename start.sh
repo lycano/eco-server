a91cb679c602451b66cbd345c70c7e193cfd204c
Lycano <luricos@gmx.de>
2018-06-17 13:56:12 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,19 +3,19 @@
 if [ ! -f Configs/WorldGenerator.eco ]; then
   echo "Copying default configs"
   mkdir -p Configs
-  cp DefaultConfigs/* Configs/
+  cp -varf DefaultConfigs/* Configs/
 fi
 
 if [ ! -f Storage/DefaultWorld ]; then
   echo "Copying default storage"
   mkdir -p Storage
-  cp DefaultStorage/* Storage/
+  cp -varf DefaultStorage/* Storage/
 fi
 
 if [ ! -d Mods/. ]; then
   echo "Copying default mods"
   mkdir -p Mods
-  cp DefaultMods/* Mods/
+  cp -varf DefaultMods/* Mods/
 fi
 
 echo "$ECO_VESRION" > Configs/eco_version
